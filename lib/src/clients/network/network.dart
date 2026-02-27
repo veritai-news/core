22d63162293ec823de36659a7eedd3f91ce1396f
Fulleni Flen <fulleni@hotmail.com>
2026-02-27 11:21:37 +0100

    feat(network): export Dio types for external use
    - Add export statements for major Dio types in the network client
    - This change allows other parts of the application to use Dio types directly
    - Improves code organization and reduces duplication

--- lib/src/clients/network/network.dart
+++ lib/src/clients/network/network.dart
@@ -1,3 +1,14 @@
+// Export Dio types
+export 'package:dio/dio.dart'
+    show
+        CancelToken,
+        DioException,
+        FormData,
+        Interceptor,
+        InterceptorsWrapper,
+        MultipartFile,
+        Options;
+
 export 'adapters/adapters.dart';
 export 'http_client.dart';
 export 'interceptors/interceptors.dart';
